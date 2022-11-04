39b536071d5cc43887a6ed705ce6ee15f6f62dea
aadil-704 <111413328+aadil-704@users.noreply.github.com>
2022-11-04 18:30:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+
 import plotly.express as px
 import plotly.graph_objects as go
-import geopandas as gpd
+
 import streamlit as st
 import plotly.offline as pyo
 
