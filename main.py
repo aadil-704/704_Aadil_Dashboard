e444e1d4182f78257f9ad5576e00e3656e41513d
aadil-704 <111413328+aadil-704@users.noreply.github.com>
2023-10-30 11:19:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -98,13 +98,13 @@ with tab2:
   
   with col1:
     option = st.selectbox(
-      'How would you like to be contacted?',
+      'Select State 👇',
       (a))
   df1=df[(df['STATE'] == option)]
   with col2:
     b=df1.CONSTITUENCY.unique()
     option2 = st.selectbox(
-      'How would you ',
+      'Select Constituency 👇',
       (b))
   
   df2=df1[(df1['STATE'] == option) & (df1['CONSTITUENCY'] == option2)]
