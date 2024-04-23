3558e53f40680aa4cc7e3c9b2f58abbfcf3357e3
aadil-704 <111413328+aadil-704@users.noreply.github.com>
2024-04-24 00:21:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,16 +52,14 @@ if tab_selector == "Graph":
     winner_education = df_winners['EDUCATION'].value_counts().reset_index()  # Count winners' education levels
     winner_education.columns = ['EDUCATION', 'COUNT']
 
-    # Print out the contents of the winner_education DataFrame
-    print(winner_education)
-
+    # Plot the bar chart for winning candidates' educational degrees without specifying color
     fig = px.bar(winner_education, 
-             x='EDUCATION', 
-             y='COUNT', 
-             title='Winning Candidates Educational Degree',
-             hover_data={'EDUCATION': False, 'COUNT': True, 'PARTY': True, 'STATE': True, 'CONSTITUENCY': True})
-    st.plotly_chart(fig)  # Added a closing parenthesis here
-    
+                 x='EDUCATION', 
+                 y='COUNT', 
+                 title='Winning Candidates Educational Degree',
+                 hover_data={'EDUCATION': False, 'COUNT': True, 'PARTY': True, 'STATE': True, 'CONSTITUENCY': True})
+    st.plotly_chart(fig)
+
     # Convert 'Criminal' column to numeric
     df['Criminal'] = pd.to_numeric(df['Criminal'], errors='coerce')
     df['Criminal'] = df['Criminal'].fillna(0)
