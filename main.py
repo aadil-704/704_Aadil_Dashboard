3506b5538edf9a79959fa5ee9c7346874463c106
aadil-704 <111413328+aadil-704@users.noreply.github.com>
2024-04-23 16:44:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if tab_selector == "Graph":
     individual_criminal_cases = df.groupby('NAME')['Criminal'].sum().reset_index()
     individual_criminal_cases = individual_criminal_cases.sort_values(by='Criminal', ascending=False).head(10)
 
-    fig = px.bar(individual_criminal_cases, x='NAME', y='Criminal', color='PARTY', title='Top 10 Individuals with the Most Criminal Cases')
+    fig = px.bar(individual_criminal_cases, x='NAME', y='Criminal', color='Criminal', title='Top 10 Individuals with the Most Criminal Cases')
     st.plotly_chart(fig)
 
     # Clean up 'ASSETS' column and convert to integer
