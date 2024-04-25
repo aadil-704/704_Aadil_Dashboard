a0da0ced6dd0fea44e2e2e5f261b4ec50b145923
aadil-704 <111413328+aadil-704@users.noreply.github.com>
2024-04-25 16:50:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import plotly.express as px
 import streamlit as st
 
 # Streamlit UI
-selected_tab = st.sidebar.radio("Select Tab", ("Graph", "Analysis"), index=0)
+st.sidebar.header("General Elections In India 2019")
+tab_selector = st.sidebar.radio("Select Tab", ("Graph", "Analysis"))
 
-if selected_tab == "Graph":
-    st.sidebar.header("General Elections In India 2019")
+if tab_selector == "Graph":
     st.subheader("Graph")
     df = pd.read_csv("data.csv")
     df = df.rename(columns={"CRIMINAL\nCASES": "Criminal", "GENERAL\nVOTES": "General_votes", "POSTAL\nVOTES": "Postal_votes", "TOTAL\nVOTES": "Total_votes"})
@@ -106,17 +106,48 @@ if selected_tab == "Graph":
     # Sort by count of assets in descending order and select top 10
     individual_assets = individual_assets.sort_values(by='ASSETS', ascending=False).head(10)
 
-    # Plot the bar chart
-    fig_individual_assets = px.bar(individual_assets, x='NAME', y='ASSETS', color='PARTY', 
-                                 hover_data=['PARTY', 'STATE', 'CONSTITUENCY'], 
-                                 title='Top 10 Individuals with the Highest Assets', 
-                                 template='plotly_dark')
-
-    # Display the bar chart
+    # Plot the scatter plot
+    fig_individual_assets = px.scatter(individual_assets, x='NAME', y='ASSETS', color='PARTY', hover_data=['PARTY', 'STATE', 'CONSTITUENCY'], title='Top 10 Individuals with the Highest Assets', template='plotly_dark')
     st.plotly_chart(fig_individual_assets)
+   
 
-elif selected_tab == "Analysis":
-    st.sidebar.header("General Elections In India 2019")
+    # Filter and count overall category
+    cat_overall = vote[vote['PARTY'] != 'NOTA']['CATEGORY'].value_counts().reset_index()
+    cat_overall.columns = ['CATEGORY', 'Counts']
+    cat_overall['Category'] = 'Overall Category Counts'
+
+    # Filter and count winning category
+    cat_winner = vote[vote['WINNER'] == 1]['CATEGORY'].value_counts().reset_index()
+    cat_winner.columns = ['CATEGORY', 'Counts']
+    cat_winner['Category'] = 'Winning Category Ratio'
+
+    # Concatenate overall and winning category counts
+    cat_overl_win = pd.concat([cat_winner, cat_overall])
+
+    # Plot the bar chart
+    fig = px.bar(cat_overl_win, x='CATEGORY', y='Counts', color='Category', barmode='group')
+    fig.update_layout(title_text='Participation vs Win Counts for the Category in Politics', template='plotly_dark')
+    st.plotly_chart(fig)
+
+    # Filter to include only winning politicians
+    winners = df[df['WINNER'] == 1]
+
+    # Define the age ranges or bins for the histogram
+    age_bins = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+    # Create a histogram of age distribution for winning politicians with color based on gender
+    fig = px.histogram(winners, x="AGE", nbins=len(age_bins), color="GENDER", title='Age Distribution of Winning Politicians by Gender', template='plotly_dark')
+
+    # Update the layout
+    fig.update_layout(xaxis_title="Age",
+                  yaxis_title="Count",
+                  title_text='Age Distribution of Winning Politicians by Gender',
+                  template='plotly_dark')
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+elif tab_selector == "Analysis":
     st.subheader("Analysis")
     df = pd.read_csv("data.csv")
     df = df.rename(columns={"CRIMINAL\nCASES": "Criminal", "GENERAL\nVOTES": "General_votes", "POSTAL\nVOTES": "Postal_votes", "TOTAL\nVOTES": "Total_votes"})
